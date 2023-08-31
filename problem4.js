c3f9d790cba914a2bb948b9d6807e8f585f5779e
Sharmasuchi1997 <sharmasuchi1997@gmail.com>
2023-09-01 01:28:46 +0530

    Introduction to Javascript

---
+++ problem4.js
@@ -0,0 +1,17 @@
+let name="Sachin Tendulkar";
+let school="St. Sebastine";
+let grade=5;
+let section="B";
+let roll_no="35";
+let subject="Maths English Physics";
+let marks="90     85      74";
+console.log(">>>>>>>>>>>>>>>REPORT CARD<<<<<<<<<<<<<<<<")
+console.log("Name:-"+   name)
+console.log("School:-"+ school)
+console.log("Grade:-"+ grade)
+console.log("Section:-"+ section)
+console.log("Roll No:-"+ roll_no)
+console.log("Subjects:-")
+console.log(subject)
+console.log(marks)
+console.log(">*********>********>********>********>*******")
